931ff9fc45524c82fee72dc0c1442b59d4c79e6e
Tiago Silva <up201806516@fe.up.pt>
2020-09-30 19:47:40 +0100

    Finished pre-processing

--- TP1/MyNode.js
+++ TP1/MyNode.js
@@ -10,13 +10,15 @@ class MyNode {
     
     /**
      * Constructor
+     * @param {string} nodeId
      * @param {string} materialId 
      * @param {string} textureId 
      * @param {int[]} transformationMatrix 
      * @param {XMLscene} xmlScene 
      * @param {string[]} descendants
      */
-    constructor(materialId, textureId, aft, afs, transformationMatrix, xmlScene, descendants) {
+    constructor(nodeId, materialId, textureId, aft, afs, transformationMatrix, xmlScene, descendants) {
+        this.id = nodeId;
         this.materialId = materialId;
         this.material = null;
         this.materialStatus = null;
@@ -30,19 +32,70 @@ class MyNode {
         this.children = descendants;
     }
 
-    preProcess() {
+    /**
+     * Preprocesses a given node
+     * @param {MySceneGraph} graphScene 
+     */
+    preProcess(graphScene) {
+        // Process material
+        if (this.materialId === "null") {
+            this.materialStatus = MaterialStatus.KEEP;
+        }
+        else if (ALLOW_CLEAR_MATERIAL && this.material === "clear") {
+            this.materialStatus = MaterialStatus.DEFAULT;
+        }
+        else if ((this.material = graphScene.getMaterial(this.materialId)) === undefined) {
+            graphScene.onXMLMinorError("Node with id '" + this.id + "' has an invalid texture: Using parent's appearance");
+            this.materialStatus = MaterialStatus.DEFAULT;
+        }
+        
+        // Process texture
+        if (this.textureId === "null") {
+            this.textureStatus = TextureStatus.KEEP;
+        }
+        else if (this.textureId === "clear") {
+            this.textureStatus = TextureStatus.DEFAULT;
+        }
+        else if ((this.texture = graphScene.getTexture(this.textureId)) === undefined) {
+            graphScene.onXMLMinorError("Node with id '" + this.id + "' has an invalid texture: Using default texture");
+            this.textureStatus = TextureStatus.DEFAULT;
+        }
 
+        // Process descendants
+        var children = this.children;
+        this.children = [];
+        var aNode = null;
+        for (let i = 0; i < children.length; i++) {
+            if (typeof child[i] !== String) {
+                // It's a leaf node (primitive)
+                this.children.push(child[i]);
+            }
+            else {
+                // It's another regular node
+                if ((aNode = graphScene.getNode(child[i])) === undefined) {
+                    graphScene.onXMLMinorError("Could not find descendant with id '" + child[i] + "' whilst pre-processing node with id '" + this.id + "': Ignoring descendant");
+                }
+                else {
+                    this.children.push(aNode);
+                }
+            }
+        }
+
+        if (this.children.length == 0) {
+            this.scene.onXMLMinorError("TO DO\nEliminate nodes with no descendants due to errors\nTO DO\n");
+        }
+        
     }
 
     display() {
 
-
-        if (this.materialStatus == MaterialStatus.SET){
-            this.scene.pushMaterial(this.material);
-        } else if (this.materialStatus == MaterialStatus.DEFAULT){
-            this.scene.pushMaterial(this.scene.defaultAppearance);
-        } else if (this.materialStatus == MaterialStatus.KEEP){
-            //Do nothing
+        switch (this.materialStatus) {
+            case MaterialStatus.SET:
+                this.scene.pushMaterial(this.material);
+            case MaterialStatus.DEFAULT:
+                this.scene.pushMaterial(this.scene.defaultAppearance);
+            default:
+                break;
         }
 
         if (this.textureStatus == TextureStatus.SET){
