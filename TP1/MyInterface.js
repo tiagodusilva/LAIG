a9deaa3903983e1981d8d9cdda7464b97a7f3a4c
Tiago Silva <up201806516@fe.up.pt>
2020-10-02 20:24:49 +0100

    Fixed ortho camera

--- TP1/MyInterface.js
+++ TP1/MyInterface.js
@@ -30,8 +30,8 @@ class MyInterface extends CGFinterface {
         this.gui = new dat.GUI();
 
         this.gui.add(this.scene, 'showAxis').name("Show Axis");
-        this.gui.add(this.scene, 'selectedCamera', this.scene.cameraDropdown).onChange(this.scene.onCameraChange.bind(this.scene)).name('Camera');
         this.gui.add(this.scene, 'showNormals').onChange(this.scene.changeNormalViz.bind(this.scene)).name('Show Normals');
+        this.gui.add(this.scene, 'selectedCamera', this.scene.cameraDropdown).onChange(this.scene.onCameraChange.bind(this.scene)).name('Camera');
 
         var lightFolder = this.gui.addFolder("Illumination");
         for (let i = 0; i < this.scene.lightCount; i++){
