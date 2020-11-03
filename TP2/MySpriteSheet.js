e29abca821be06b0f77fbd045550984d39d8aac4
Ricardo Fontão <up201806317@fe.up.pt>
2020-11-03 12:18:32 +0000

    display for text

--- TP2/MySpriteSheet.js
+++ TP2/MySpriteSheet.js
@@ -1,3 +1,8 @@
+const TEXT_SPRITE_SHEET = "a";
+const TEXT_SPRITE_SHEET_M = 16;
+const TEXT_SPRITE_SHEET_N = 8;
+
+
 class MySpriteSheet {
     constructor(texture, sizeM, sizeN){
         this.texture = texture;
@@ -18,11 +23,12 @@ class MySpriteSheet {
 
 class MySpriteText {
 
-    static spriteSheetName = new MySpriteSheet("a", 5, 5);
+    static textSheet = new MySpriteSheet(TEXT_SPRITE_SHEET, TEXT_SPRITE_SHEET_M, TEXT_SPRITE_SHEET_N);
 
     constructor(scene, text){
         this.scene = scene;
         this.text = text;
+        this.rectangle = new MyRectangle(scene, 0, 0, 1, 1, 1, 1);
     }
 
     getCharacterPosition(character) {
@@ -30,7 +36,16 @@ class MySpriteText {
     }
 
     display() {
+        let startMatrix = this.scene.activeMatrix;
+        let translationMatrix;
+        for(let character of this.text){
+            this.textSheet.activateCellP(this.getCharacterPosition(character));
+            this.rectangle.display();
+            mat4.fromTranslation(translationMatrix, Vec3(1, 0, 0));
+            this.scene.multMatrix(translationMatrix);
+        }
 
+        this.scene.setActiveMatrix(startMatrix);
     }
 
 }
