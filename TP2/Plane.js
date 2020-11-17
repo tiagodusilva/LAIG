d01897b1c92d1dda0711c8ce8eac9bae4390b014
Ricardo Fontão <up201806317@fe.up.pt>
2020-11-17 11:14:02 +0000

    Fixed plane not showing

--- TP2/Plane.js
+++ TP2/Plane.js
@@ -5,18 +5,19 @@ class Plane extends CGFobject {
         this.divsV = divsV;
         this.controlPoints = [
             [
-                [-0.5, 0, 0.5],
-                [0.5, 0, 0.5]
+                [0.5, 0, 0.5, 1],
+                [-0.5, 0, 0.5, 1]
             ],
             [
-                [-0.5, 0, -0.5],
-                [0.5, 0, -0.5]
+                [0.5, 0, -0.5, 1],
+                [-0.5, 0, -0.5, 1]
             ]
         ];
         this.nurbs = new CGFnurbsObject(this.scene, this.divsU, this.divsV, new CGFnurbsSurface(1, 1, this.controlPoints));
     }
 
     display(){
+        console.log("HELLO")
         this.nurbs.display();
     }
 
