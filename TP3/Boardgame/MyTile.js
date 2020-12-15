94181411dfb2e79077f4306560b34180fdb3cb95
Ricardo Fontão <up201806317@fe.up.pt>
2020-12-15 11:04:19 +0000

    Small fix

---
+++ TP3/Boardgame/MyTile.js
@@ -0,0 +1,51 @@
+class MyTile {
+    constructor(scene, gameboard) {
+        this.scene = scene;
+        this.gameboard = gameboard; 
+        this.stack = []; /* stack is the name used in our prolog game */
+        this.tile = new Plane(scene, 2, 2);
+        this.uniqueId = this.scene.currentUniqueId++;
+        this.selectable = true;
+    }
+
+    addPiece(piece){
+        let topPiece = this.getTopPiece();
+        if(topPiece){
+            topPiece.selectable = false;
+        }
+        piece.selectable = true;
+        this.stack.push(piece);
+    }
+
+    removePiece() {
+        let pieceToRemove = this.stack.pop();
+        pieceToRemove.selectable = false;
+
+        let topPiece = this.getTopPiece();
+        if(topPiece){
+            topPiece.selectable = true;
+        }
+        return pieceToRemove;
+    }
+
+    getTopPiece() {
+        return this.stack.length == 0 ? null : this.stack[this.stack.length - 1];
+    }
+
+    display() {
+        if (this.selectable)
+            this.scene.registerForPick(this.uniqueId, this);
+        this.tile.display();
+        if (this.selectable)
+            this.scene.clearPickRegistration();
+
+        for (let i = 0; i < this.stack.length; i++) {
+            this.scene.pushMatrix();
+            this.scene.rotate(-Math.PI/2, 1, 0, 0);
+            this.scene.translate(0, 0, 0.25 * (i + 1));
+            this.stack[i].display();
+            this.scene.popMatrix();
+        }
+
+    }
+}
